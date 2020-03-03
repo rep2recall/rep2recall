02583b00623155b22ccc8849585cdc7f4de69215
Pacharapol Withayasakpunt <patarapolw@gmail.com>
2020-03-03 16:08:23 +0700

    make sure electron works when packaged

--- packages/electron/src/utils.ts
+++ packages/electron/src/utils.ts
@@ -4,7 +4,7 @@ import path from 'path'
 import { app } from 'electron'
 
 export const MEDIA_PATH = path.join(app.getPath('userData'), 'media')
-export const WEB_PATH = path.resolve('lib/web')
+export const WEB_PATH = path.resolve(__dirname, '../lib/web')
 
 if (!fs.existsSync(MEDIA_PATH)) {
   fs.mkdirSync(MEDIA_PATH, { recursive: true })
