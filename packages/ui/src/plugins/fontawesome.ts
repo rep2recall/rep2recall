6c4c2aa553b3ece3cbe3c14a5db8090f085c1434
Pacharapol Withayasakpunt <patarapolw@gmail.com>
2020-03-23 19:23:31 +0700

    update ui

--- packages/ui/src/plugins/fontawesome.ts
+++ packages/ui/src/plugins/fontawesome.ts
@@ -2,14 +2,14 @@ import Vue from 'vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import {
   faFileImport, faUsers, faCog, faUser,
-  faCaretRight, faCaretDown,
+  faCaretRight, faCaretDown, faList,
 } from '@fortawesome/free-solid-svg-icons'
 import { faQuestionCircle, faEdit } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(
   faFileImport, faUsers, faCog, faUser,
-  faCaretRight, faCaretDown,
+  faCaretRight, faCaretDown, faList,
   faQuestionCircle, faEdit,
 )
 
