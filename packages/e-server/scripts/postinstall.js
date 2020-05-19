6a78e1aa9384c547bf70140d32043098502464f5
Pacharapol Withayasakpunt <patarapolw@gmail.com>
2020-05-19 09:18:18 +0700

    build to exe

--- packages/e-server/scripts/postinstall.js
+++ packages/e-server/scripts/postinstall.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const { spawnSync } = require('child_process')
 
 const pkg = require('../../e-app/package.json')
 const { dependencies } = require('../package.json')
@@ -7,3 +8,8 @@ const { dependencies } = require('../package.json')
 pkg.dependencies = Object.assign(pkg.dependencies, dependencies)
 
 fs.writeFileSync(path.join(__dirname, '../../e-app/package.json'), JSON.stringify(pkg, null, 2))
+
+spawnSync('npm', ['i'], {
+  cwd: path.join(__dirname, '../../e-app'),
+  stdio: 'inherit'
+})
