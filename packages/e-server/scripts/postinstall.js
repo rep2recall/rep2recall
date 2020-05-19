66c7ca3746ca430ab9f7fbea09f4ced72c00a8a9
Pacharapol Withayasakpunt <patarapolw@gmail.com>
2020-05-19 11:56:30 +0700

    correct CI

--- packages/e-server/scripts/postinstall.js
+++ packages/e-server/scripts/postinstall.js
@@ -5,7 +5,7 @@ const { spawnSync } = require('child_process')
 const pkg = require('../../e-app/package.json')
 const { dependencies } = require('../package.json')
 
-pkg.dependencies = Object.assign(pkg.dependencies, dependencies)
+pkg.dependencies = Object.assign(dependencies, pkg.peerDependencies)
 
 fs.writeFileSync(path.join(__dirname, '../../e-app/package.json'), JSON.stringify(pkg, null, 2))
 
