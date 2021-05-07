4c73b77c07023b1c4ed0968fd03d642577aee3e3
Pacharapol Withayasakpunt <polv@polv.cc>
2021-05-07 11:36:40 +0700

    add basic jar

---
+++ packages/jar/app/src/main/kotlin/rep2recall/api/util.kt
@@ -0,0 +1,5 @@
+package rep2recall.api
+
+data class Config(
+    val baseURL: String
+)
---
+++ packages/jar/app/src/main/kotlin/rep2recall/db/util.kt
@@ -0,0 +1,6 @@
+package rep2recall.db
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+
+val gson: Gson = GsonBuilder().create()
